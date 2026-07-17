b685c192544171b1b8896847181082dfa9b8b2a8
StahlAG <nutting89@yahoo.com>
2026-07-16 23:52:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 fetch("market-data.json")
 .then(response => response.json())
 .then(data => {
-    alert("Script Loaded");
-    console.log(data);
 
     if (data.ritzville &&
         data.ritzville.cashBids &&
